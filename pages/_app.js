1dfc056ba151f977d314b789118dbec50f03520a
AvantikaI <avantikachauhan2005@gmail.com>
2025-07-30 01:02:26 +0530

    Fix: Unified button styles across pages using .btn-global

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import '@styles/app.scss'
 import { UserContext } from '@lib/context';
 import Layout from './layout';
 import { useUserData } from '@lib/userInfo';
+import '../styles/app.scss';
+
+
 
 function MyApp({ Component, pageProps }) {
   const userData = useUserData();
