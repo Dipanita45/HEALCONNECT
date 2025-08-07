eca55f8714507e1d422436e21b4bee4ac318a039
Sayali <sayaliy331@gmail.com>
2025-08-07 22:37:33 +0530

    Dark Mode feature added

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,12 @@
-// pages/_app.js
-import '@styles/app.scss';
-/*import { useState } from 'react';
-import { UserContext } from '@/lib/context';
-import Navbar from '@/components/Navbar';
+import { ThemeProvider } from '@/context/ThemeContext'
+import Navbar from '@/components/navbar' // Import your Navbar
+import '@/styles/globals.css'
+
 export default function App({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-  const [userRole, setUserRole] = useState(null);
-  const [currentUser, setCurrentUser] = useState(null);
-
   return (
-
-    <UserContext.Provider value={{ user, setUser, userRole, setUserRole, currentUser, setCurrentUser }}>
-       <Navbar />     
+    <ThemeProvider>
+      <Navbar /> {/* Add Navbar here */}
       <Component {...pageProps} />
-    </UserContext.Provider>
-  );
+    </ThemeProvider>
+  )
 }
-*/
-// src/App.jsx
-// pages/_app.js
-// pages/_app.js
-import '../styles/globals.css';  // global styles
-import Navbar from '../components/navbar';
-import '../components/Navbar.module.css'
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  );
-}
-
