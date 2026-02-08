4f819894cfa24cd495ca9d3346e41707ddb14033
stargalax <nikitha.s.2022.cse@ritchennai.edu.in>
2026-02-08 11:01:14 +0530

    fixed dr. emily img

--- components/home/Doctors.jsx
+++ components/home/Doctors.jsx
@@ -27,7 +27,7 @@ export default function Doctors() {
                                     src={doctor.image}
                                     alt={doctor.name}
                                     fill
-                                    className="object-cover transition-transform duration-500 group-hover:scale-110"
+                                    className="object-top transition-transform duration-500 group-hover:scale-110"
                                 />
                                 <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
                             </div>
